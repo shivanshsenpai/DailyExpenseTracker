3519d1c094585cc5938a8a4b00e7c941d80d42b5
shivanshsenpai <147553887+shivanshsenpai@users.noreply.github.com>
2026-04-02 03:17:04 +0530

    fix signup url

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -18,7 +18,7 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("/signup/", {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/signup/`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
